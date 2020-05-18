a0dfed3f6c45c83250341d8fb9e075cfbe17ae1f
Michael G Rivera <rivera.mgr@gmail.com>
2020-05-18 19:02:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index)
+  board[index_number]
+  if index == "  "
+    "false"
+  elsif index == " "
+    "false"
+  else
+    "true"
+
+end
